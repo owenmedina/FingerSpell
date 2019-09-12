4c575fe60a76a1e376acb0c9c586e77f9e340de4
Brian Pacia <brianjeffreyppacia@yahoo.com>
2019-09-12 13:44:06 +0800

    Fix sql statement

--- src/LeapDB.java
+++ src/LeapDB.java
@@ -99,7 +99,7 @@ public class LeapDB {
 			// first inner array is for the x, y and z of the tip of the finger
 			// second inner array is for the x, y and z of the proximal joint of the finger
 			
-			PreparedStatement newGesture = con.prepareStatement("INSERT INTO gestures VALUES('" + name +
+			PreparedStatement newGesture = con.prepareStatement("INSERT INTO gestures VALUES('" + name + "', '"
 						+ tTip_tProx + "', '" + tTip_iTip + "', '" + tTip_iProx + "', '" + tTip_mTip + "', '" + tTip_mProx + "', '" + tTip_rTip + "', '"
 						+ tTip_rProx + "', '" + tTip_pTip + "', '" + tTip_pProx + "', '" + tTip_pc + "', '" + tProx_iTip + "', '" + tProx_iProx + "', '"
 						+ tProx_mTip + "', '" + tProx_mProx + "', '" + tProx_rTip + "', '" + tProx_rProx + "', '" + tProx_pTip + "', '" + tProx_pProx + "', '"
