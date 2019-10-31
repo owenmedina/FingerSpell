cccd8ef2e5ee98577a0915b12ab920ee9620de48
Brian Pacia <brianjeffreyppacia@yahoo.com>
2019-10-31 15:46:27 +0800

    Integrate SVC model into Java code  - Use scikit Porter to export SVM model into a Java class  - Add connection to MySQL db  - Add code to test if prediction works on Java side (it does)

--- src/LeapController.java
+++ src/LeapController.java
@@ -52,6 +52,20 @@ public class LeapController {
 			}
 			
 
+		}else if(input.equals("z")) {
+			Frame frame = controller.frame();
+			int counter = 0;
+			//String[] features =  listener.getFeatures("",counter,1,frame);
+			double[] feats = {0,0,0,40.62068,59.42944717,92.17320251,80.74962616,39.74961472,37.55830765,0.186537951,0.270516723,0.315542698,0.020274311,0.280239016,44.4185791,44.41504669,65.62532806,69.994133,65.4125061,106.383728,107.3475266,68.29267883,69.8995285,14.13694477
+			};
+			String[] s = new String[feats.length];
+			for (int i = 0; i < s.length; i++)
+			    s[i] = String.valueOf(feats[i]);
+			SVC.main(s);
+			double[] scaled = calculator.scaleFeatures(feats);
+			System.out.println(Arrays.toString(scaled));
+			
+
 		}
 				
 //		try {
@@ -62,6 +76,8 @@ public class LeapController {
 		
 		controller.removeListener(listener); 
 	}
+	
+	
 	
 
 }
